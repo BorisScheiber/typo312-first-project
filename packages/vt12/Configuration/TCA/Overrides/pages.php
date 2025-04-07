c6bcc3305d713f56bbd2b3d415ac770323671b82
Boris Scheiber <scheiberboris@hotmail.com>
2025-04-08 01:52:09 +0200

    learning how to create a custom extension

--- packages/vt12/Configuration/TCA/Overrides/pages.php
+++ packages/vt12/Configuration/TCA/Overrides/pages.php
@@ -8,4 +8,10 @@ defined('TYPO3') or die('Access denied.');
     'Default Backendlayouts' // Titel, wie er im Backend angezeigt wird
 );
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'vt12',
+    'Configuration/TsConfig/Page/general.tsconfig',
+    'General Page TsConfig' // Titel, wie er im Backend angezeigt wird
+);
+
 
